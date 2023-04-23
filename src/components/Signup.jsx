556eb32d4cddebd3b9016ebe09f777eaf146489d
andresmgomez <andresmgomez23@gmail.com>
2023-04-23 12:53:39 -0400

    Create a Signup component with html markup

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 
+import "./Signup.css";
+
 function Signup() {
-	return (
-		<section id="signup-users">
-			{/* TODO: Add html markup for a register form */}
+   return (
+		<section id="sign-up">
+			<div className="container">
+				<div className="left-content">
+					<div className="register-form">
+						{/* TODO: Add the html markup for a register page */}
+					</div>
+				</div>
+				<div className="right-content">
+					<div className="info-message">
+						{/* TODO: Add the html markup for a heading message */}
+					</div>
+				</div>
+			</div>
 		</section>
 	);
 }
