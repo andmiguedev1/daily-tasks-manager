167f5aa32aca57d8620dd68f12ef4b9cef9435aa
andresmgomez <andresmgomez23@gmail.com>
2023-04-23 10:47:39 -0400

    Remove unnecessary image and styles

---
+++ src/components/Signup.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Signup() {
+	return (
+		<section id="signup-users">
+			{/* TODO: Add html markup for a register form */}
+		</section>
+	);
+}
+
+export default Signup;
