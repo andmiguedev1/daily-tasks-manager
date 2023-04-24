63fdecfdcb61411d6a9282635614d02af1a3fdff
andresmgomez <andresmgomez23@gmail.com>
2023-04-24 06:28:23 -0400

    Add markup for register form and heading message

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -8,12 +8,31 @@ function Signup() {
 			<div className="container">
 				<div className="left-content">
 					<div className="register-form">
-						{/* TODO: Add the html markup for a register page */}
+						{/* TODO: Add the html markup for a register form */}
+						<form>
+							<h2>Register an account</h2>
+							<div className="form-fields">
+								<input type="text" placeholder="Name" />
+								<input type="text" placeholder="Username" required />
+								<input type="email" placeholder="Email" required />
+								<input type="password" placeholder="Password" required />
+								<input type="password" placeholder="Confirm Password" required />
+							</div>
+							<div className="signup-btn">
+								<button type="submit">Sign Up</button>
+							</div>
+						</form>
 					</div>
 				</div>
 				<div className="right-content">
-					<div className="info-message">
+					<div className="heading-message">
 						{/* TODO: Add the html markup for a heading message */}
+						<h2>Your tasks</h2>
+						<p>
+							Organize your daily activities by adding, changing, and removing your
+							daily tasks, and kep important tasks on the top.
+						</p>
+						<p class="redirect">Already have an account ?</p>
 					</div>
 				</div>
 			</div>
