d3542a5eb8b3c4eb86273265ae14b148fac5a538
andresmgomez <andresmgomez23@gmail.com>
2023-04-28 10:21:50 -0400

    Add functionality to register and login an online user

--- src/state/context/authContext.js
+++ src/state/context/authContext.js
@@ -1,27 +1,57 @@
 import { useContext, useState } from "react";
+import { auth } from "../../private/firebaseConfig";
 
-// TODO: Add a variable that stores AuthContext
 const AuthContext = React.createContext();
 
-// TODO: Create a custom hook function that returns
-// the created context for firebase authentication
 export function useFirebaseAuth() {
 	return useContext(AuthContext);
 }
 
-// TODO: Create a function that exports AuthProvider
-// and pass the React children prop as a parameter
 export function AuthProvider({ children }) {
-	// TODO: Destructure the variables "onlineUser"
-	// and "loadingState" with its setter function
 	const [onlineUser, setOnlineUser] = useState([]);
 	const [loadingState, setLoadingState] = useState(true);
 
-	// TODO: Return a special component that calls
-	// the Provider inside the AuthContext, then
-	// pass onlineUser as an argument inside value
+	// TODO: Create an async function that register an
+	// online user's email and password in firebase auth
+	//
+	// Note: Make sure to import auth from firebase config
+	// and then call the built-in firebase method
+	async function register(userEmail, userPassword) {
+		try {
+			if (userEmail && userPassword) {
+				await auth.createUserWithEmailAndPassword(userEmail, userPassword);
+			}
+		} catch (err) {
+			console.log(
+				"Sorry! We were unable to sign you in. Check error message:",
+				error.message
+			);
+		}
+	}
+
+	// TODO: Create a function that login a registered
+	// online user
+	async function login(userEmail, userPassword) {
+		try {
+			if (userEmail && userPassword) {
+				await auth.signInWithEmailAndPassword(userEmail, userPassword);
+			}
+		} catch (err) {
+			console.log("Your credentials do not match our records. Try again!");
+		}
+	}
+
+	// TODO: Add an object that holds the auth functions
+	// and user info so, that it can passed more than one
+	// value as an argument
+	const authInfo = {
+		onlineUser,
+		register,
+		login,
+	};
+
 	return (
-		<AuthContext.Provider value={onlineUser}>
+		<AuthContext.Provider value={authInfo}>
 			{!loadingState && children}
 		</AuthContext.Provider>
 	);
